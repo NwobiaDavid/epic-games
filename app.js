21c9f7d90673be60a0cf2ba997f7ddfab30e5f59
NwobiaDavid <dnwobia@gmail.com>
2023-03-08 00:19:21 +0100

    still working on navbar responsivity

---
+++ app.js
@@ -0,0 +1,9 @@
+$("#responive-toggle").on("click",()=>{ 
+   $(".responsive-toggle").toggle();
+   $(".ex").toggleClass(".side-bar-active");
+   
+})
+
+$("#ex").on("click",()=>{
+
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js}",
+    "css/one.css"
   ],
   theme: {
     extend: {},
