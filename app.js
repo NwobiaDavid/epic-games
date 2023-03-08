39bd0fc7ff8c143be363589114d6745f192170d3
NwobiaDavid <dnwobia@gmail.com>
2023-03-08 15:33:17 +0100

    done with navbar

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-$("#responive-toggle").on("click",()=>{ 
-   $(".responsive-toggle").toggle();
-   $(".ex").toggleClass(".side-bar-active");
+$(document).ready(
+   ()=>{
+      alert("ready");
+   }
+)
+
+$("#responsive-toggle").on("click",()=>{
    
-})
+//   if( $("#responsive-toggle").attr("class").includes("flex")){
+//    $("#responsive-toggle").toggleClass("flex");
+//    $("#responsive-toggle").toggleClass("hidden");
+//    $("#ex").toggleClass("flex");
+//    $("#ex").toggleClass("hidden");
+//   }else $("#responsive-toggle").addClass("flex");
+$("#responsive-toggle").toggleClass("flex");
+$("#responsive-toggle").toggleClass("hidden");
+$("#ex").toggleClass("hidden");
+$("#ex").toggleClass("flex");
+
+});
+
+
+$("#responsive-toggle").on("click", function () {
+   $("#side-bar").toggleClass("flex");
+   // $("#side-bar").animate({ right: "0" }, 500);
+   // console.log("clicked");
+ });
 
 $("#ex").on("click",()=>{
+   $("#responsive-toggle").toggleClass("flex");
+   $("#responsive-toggle").toggleClass("hidden");
+   $("#ex").toggleClass("hidden");
+   $("#ex").toggleClass("flex");
+});
 
-})
+ $("#ex").on("click", function () {
+   $("#side-bar").toggleClass("flex");
+   // $("#side-bar").fadeOut();
+   // $("#side-bar").animate({ right: "-100%" }, 500);
+ });
