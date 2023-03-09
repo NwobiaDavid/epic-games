89f324bbb1755e6eb65c907c6b2f6893b670ebb4
NwobiaDavid <dnwobia@gmail.com>
2023-03-09 19:56:30 +0100

    added the homepage carousel

--- app.js
+++ app.js
@@ -38,3 +38,56 @@ $("#ex").on("click",()=>{
    // $("#side-bar").fadeOut();
    // $("#side-bar").animate({ right: "-100%" }, 500);
  });
+
+
+ //create carousel with captions using tailwind and jquery?
+
+//  let slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//    showSlides(slideIndex = n);
+//  }
+
+//  function showSlides(n) {
+//    let i;
+//    let slides = document.getElementsByClassName("mySlides");
+//    let dots = document.getElementsByClassName("dot");
+
+//    if (n > slides.length) {slideIndex = 1}
+//    if (n < 1) {slideIndex = slides.length}
+
+//    for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+
+//     for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "flex";
+//     dots[slideIndex-1].className += " active";
+//   }
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+    }
+    dots[slideIndex-1].className += " active";
+
+  setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/**/*.{html,js}",
     "css/one.css"
   ],
+  plugins: [require("daisyui")],
   theme: {
     extend: {},
   },
